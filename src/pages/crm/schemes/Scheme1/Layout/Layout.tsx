62053ea2000ece4f9cecc88a0822e81121707617
dashaBobrovad <dbobrova.dasha@gmail.com>
2023-08-31 23:20:53 +0300

    scheme 1 markup is done

--- src/pages/crm/schemes/Scheme1/Layout/Layout.tsx
+++ src/pages/crm/schemes/Scheme1/Layout/Layout.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import cx from './index.module.scss';
-import { Image, Typography } from "../../../../../components/ui";
+import { Button, Image, Typography } from "../../../../../components/ui";
+import { ReactComponent as RightArrowIcon } from '../../../../../../static/images/icons/arrows/right.svg';
 
 export default function Layout() {
   return (
-    <div >
+    <div className={cx.wrapper}>
       <div className={cx.top}>
         <div className={cx.title}>
           <Typography variant="h2">Balenciaga</Typography>
@@ -12,10 +13,30 @@ export default function Layout() {
         </div>
         <div className={cx.pic}>
           <Image src={process.env.VITE_PUBLIC_PLACEHOLDER_IMAGE} alt="placeholder" className={cx.image} />
-          <div className={cx.links}><p>text</p></div>
+          <div className={cx.links}><ul className={cx.list}><li><p>text</p></li><li><p>text</p></li><li><p>text</p></li></ul><Button variant="contained" endIcon={<RightArrowIcon />}>больше</Button></div>
         </div>
       </div>
+
+      <div className={cx.banner}>
+        <Typography variant="h2">создай свой уникальный образ вместе с Balenciaga</Typography>
+        <Button variant="contained" endIcon={<RightArrowIcon />}>перейти к товарам</Button>
+      </div>
+
+      <div className={cx.gallery}>
+        <Image src={process.env.VITE_PUBLIC_PLACEHOLDER_IMAGE} alt="placeholder" className={cx.image} />
+        <Image src={process.env.VITE_PUBLIC_PLACEHOLDER_IMAGE} alt="placeholder" className={cx.image} />
+        <Image src={process.env.VITE_PUBLIC_PLACEHOLDER_IMAGE} alt="placeholder" className={cx.image} />
+        <Image src={process.env.VITE_PUBLIC_PLACEHOLDER_IMAGE} alt="placeholder" className={cx.image} />
+        <div className={cx.title}>
+          <Typography variant="h3">заголовок</Typography>
+          <p>основной текст</p>
+        </div>
+        <Image src={process.env.VITE_PUBLIC_PLACEHOLDER_IMAGE} alt="placeholder" className={cx.image} />
+
+      </div>
+
     </div>
+
 
   )
 }
