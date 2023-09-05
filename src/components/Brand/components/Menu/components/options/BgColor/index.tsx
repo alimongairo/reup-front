20e6e85663af75cd5a25820338f348cee796b704
dashaBobrovad <dbobrova.dasha@gmail.com>
2023-09-05 20:45:37 +0300

    bg color main templ

--- src/components/Brand/components/Menu/components/options/BgColor/index.tsx
+++ src/components/Brand/components/Menu/components/options/BgColor/index.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { useContext } from 'react';
 
-export default function BgColor() {
+import Container from '../../Container';
+import Options from './Options';
+import { MenuContext } from "../../../helpers";
+
+const BgColor = () => {
+  const menuContext = useContext(MenuContext);
   return (
-    <div>BgColor</div>
-  )
-}
+    <Container
+      title={'изменить цвет фона и кнопок'}
+      onBack={menuContext?.toHome}
+    >
+      <Options />
+    </Container>
+  );
+};
+
+export default BgColor;
