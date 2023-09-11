6b02d31abd3fcd0c8a52fd069679d027d7165b50
dashaBobrovad <dbobrova.dasha@gmail.com>
2023-09-11 20:21:21 +0300

    dx

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
 import MainPage from "./MainPage";
 import ErrorPage from "./ErrorPage";
-import SchemePage from "./SchemePage/index.tsx";
-import SchemeSelectionPage from "./SchemeSelectionPage";
+import SchemePage from "./crm/SchemePage/index.tsx";
+import SchemeSelectionPage from "./crm/SchemeSelectionPage";
 
 export {ErrorPage, MainPage, SchemePage, SchemeSelectionPage};
