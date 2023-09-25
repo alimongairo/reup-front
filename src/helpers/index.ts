6107f8f95b3e909f9f708bfb4ae6706fad93a0e9
dashaBobrovad <dbobrova.dasha@gmail.com>
2023-09-25 20:51:40 +0300

    part of bck

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -10,6 +10,7 @@ function styleParser(val: string) {
     const str = val;
     const test = "{ 'color': '#6b19e6'; 'fontFamily': 'Dela Gothic One'; 'fontSize': '16px'; 'fontStyle': 'normal'; 'fontWeight': '500'; 'lineHeight': '110%'; 'letterSpacing': '1px'}".replaceAll("'", '"').replaceAll(";", ",");
 
+    console.log(JSON.parse(test))
     return JSON.parse(test);
 }
 
