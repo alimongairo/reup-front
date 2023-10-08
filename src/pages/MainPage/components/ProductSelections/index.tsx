bb37653057f1cb2a0476fb64fd4aec0199f97a7e
dashaBobrovad <dbobrova.dasha@gmail.com>
2023-10-08 17:39:13 +0300

    fx

--- src/pages/MainPage/components/ProductSelections/index.tsx
+++ src/pages/MainPage/components/ProductSelections/index.tsx
@@ -5,7 +5,7 @@ import { useSwiper } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import cx from './index.module.scss';
-import { Button } from "../../../../components/ui";
+import { Button, MoreLink } from "../../../../components/ui";
 import SwiperConstructor, { type Swiper as SwiperRef } from 'swiper'
 import { ReactComponent as ArrowIcon } from '../../../../../static/images/icons/arrows/default.svg';
 import cls from 'classnames';
@@ -13,10 +13,6 @@ import ProductItem from "../../../../components/ProductItem";
 import { uid } from "react-uid";
 import '../../../../styles/swipe.scss';
 import { useWindowWidth } from "../../../../hooks";
-
-// TODO: 
-// подправить стили (когда ресайзим, все еще пересчитывается ширина слайдов - этого делать не надо )
-
 
 export default function ProductSelections() {
     const windowWidth = useWindowWidth();
@@ -26,25 +22,34 @@ export default function ProductSelections() {
     const swiperRef = useRef<SwiperRef>();
 
     const swiperSettings = {
-            
+
         breakpoints: {
             // when window width is >= 759px (min-width)
-            759: {
+            1700: {
                 slidesPerView: 6,
                 spaceBetween: 24,
-        }},
+            },
+            1300: {
+                slidesPerView: 5,
+                spaceBetween: 24,
+            },
+            759: {
+                slidesPerView: 4,
+                spaceBetween: 24,
+            }
+        },
     };
 
-    
-   const [swiperInstance, setSwiperInstance] = useState<any>();
-    const enableSwiper =  () => {
-   
-        
+
+    const [swiperInstance, setSwiperInstance] = useState<any>();
+    const enableSwiper = () => {
+
+
         const mySwiper = new SwiperConstructor(swiperRef.current as any, swiperSettings);
         mySwiper.init();
         setSwiperInstance(mySwiper);
     };
-    
+
 
     useEffect(() => {
         enableSwiper();
@@ -73,29 +78,27 @@ export default function ProductSelections() {
         [swiperInstance],
     )
     return (
-        <div className={cls(cx.wrapper, 'swipe')}>
-            <SwiperComponent
-                className="swipe"
-                onBeforeInit={({ el }: any) => {
+        <div className={cx.selection}>
+            <div className={cx.wrapper}>
+                <SwiperComponent
+                    className={cls(cx.swiper, "swipe")}
+                    onBeforeInit={({ el }: any) => {
 
-                    swiperRef.current = el;
-                }}
+                        swiperRef.current = el;
+                    }}
+                >
+                    {
+                        productsPlug.map((_, index) => <SwiperSlide key={uid(index)} className={cx.slide}><ProductItem size="inSwiper" /></SwiperSlide>)
+                    }
+                </SwiperComponent>
 
+                <div className={cx.nav}>
+                    <Button onClick={onPrevClick} className={cx.prevBtn}><ArrowIcon fill="#000000" stroke="#000000" /></Button>
+                    <Button onClick={onNextClick} className={cx.nextBtn}><ArrowIcon fill="#000000" stroke="#000000" /></Button>
+                </div>
 
-            >
-
-                {
-                    productsPlug.map((_, index) => <SwiperSlide key={uid(index)} className={cx.slide}><ProductItem size="inSwiper" /></SwiperSlide>)
-                }
-
-
-            </SwiperComponent>
-
-            <div className={cx.nav}>
-                <Button onClick={onPrevClick} className={cx.prevBtn}><ArrowIcon fill="#000000" stroke="#000000" /></Button>
-                <Button onClick={onNextClick} className={cx.nextBtn}><ArrowIcon fill="#000000" stroke="#000000" /></Button>
             </div>
-
+            <MoreLink className={cx.moreBtn} />
         </div>
     )
 }
