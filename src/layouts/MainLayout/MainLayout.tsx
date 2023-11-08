59e20c81bbd11703b2e0f15896f0d086659a608f
dashaBobrovad <dbobrova.dasha@gmail.com>
2023-11-08 17:52:31 +0300

    run pretrier & upl header

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -10,7 +10,7 @@ export default function MainLayout({ children }: IProps) {
     return (
         <div className={cx.wrapper}>
             {/* TODO:  1 or 2 (who is logged) */}
-            <Header type={ERoles.customer} />
+            <Header type={ERoles.seller} />
             <div className={cx.container}>{children}</div>
             <MobileToolbar />
             <Footer />
